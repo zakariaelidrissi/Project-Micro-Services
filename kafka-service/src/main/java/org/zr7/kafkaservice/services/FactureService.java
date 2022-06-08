f38ae7b485cbc4534015ce008e3844d5e58b38da
Zaki-7 <61559275+Zaki-7@users.noreply.github.com>
2022-06-09 00:36:46 +0100

    zaz

--- kafka-service/src/main/java/org/zr7/kafkaservice/services/FactureService.java
+++ kafka-service/src/main/java/org/zr7/kafkaservice/services/FactureService.java
@@ -1,13 +1,22 @@
 package org.zr7.kafkaservice.services;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.zr7.kafkaservice.entities.FactureEntity;
 import org.zr7.kafkaservice.repository.FactureRepository;
 
+import java.awt.geom.QuadCurve2D;
+import java.time.Duration;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Service
@@ -34,4 +43,16 @@ public class FactureService {
                 new Random().nextInt(2000)
         );
     }
+
+    @Bean
+    public Function<KStream<String, FactureEntity>, KStream<String, Long>> kStreamFunction(){
+        return (input)->{
+            return input
+                    .map((k,v)->new KeyValue<>(v.getClientName(), v.getFactureID()))
+                    .groupBy((k,v)->k, Grouped.with(Serdes.String(), Serdes.Long()))
+                    .count()
+                    .toStream();
+        };
+    }
+
 }
